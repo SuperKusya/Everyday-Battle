c104e387891dc8d04b852f06f37172a3322c937d
kusya <tizahodieslichto@gmail.com>
2020-10-18 16:37:30 +0300

    add some js logic

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-const cardItems = document.querySelectorAll('card__item'),
-      cardList = document.querySelector('card__list'),
-      addItemButton = document.querySelector('buttonAdd');
+const cardItems = document.querySelectorAll('.card__item'),
+      cardLists = document.querySelectorAll('.card__list'),
+      addItemButton = document.querySelector('.addButton'),
+      textField = document.querySelector('.addInput');
+
+let isInputShown = false;
+
+function addItemToList(cardList) {
+    let label = document.createElement('label'),
+        input = document.createElement('input'),
+        text = document.createElement("span");
+
+    label.appendChild(input);
+    label.appendChild(text);
+    input.setAttribute('type', 'checkbox');
+    input.classList.add('card__item');
+    text.textContent = textField.value;
+    cardList.appendChild(label);
+}
 
 
 addItemButton.addEventListener('click', function(e) {
-    let label = document.createElement('label'),
-        input = document.createElement('input');
-    label.appendChild(input);
-    cardList.appendChild(label);
-    input.setAttribute('type', 'checkbox');
-    input.classList.add('card__item');
+    if(isInputShown === false) {
+        textField.style.display = 'block';
+        isInputShown = true;
+    } else {
+        for (let cardList of cardLists) {
+            addItemToList(cardList);
+        }
+        textField.value = '';
+    }
 })
+
