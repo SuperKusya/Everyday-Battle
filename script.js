19f9cb0f1d956e702342a775c72d447c68371b72
kusya <tizahodieslichto@gmail.com>
2020-10-22 16:58:11 +0300

    add user points

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const cardLists = document.querySelectorAll('.card__list'),
       addItemButton = document.querySelector('.addButton'),
-      textField = document.querySelector('.addInput');
+      textField = document.querySelector('.addInput'),
+      labelsScore = document.querySelectorAll('.card__score'),
+      userCards = document.querySelectorAll('.card');
 
 let data = localStorage.getItem('usersData') ? JSON.parse(localStorage.getItem('usersData')) : [[], []];
+let usersScore = localStorage.getItem('usersScore') ? JSON.parse(localStorage.getItem('usersScore')) : [0, 0];
 let isInputShown = false;
 let cardItems = document.querySelectorAll('label');
 
-data.forEach((arrLabels, index) => {
-    arrLabels.forEach((labelText) => {
-        addItemToList(cardLists[index], labelText);
+function initRender() {
+    data.forEach((arrLabels, index) => {
+        arrLabels.forEach((labelText) => {
+            addItemToList(cardLists[index], labelText);
+        })
+
+        labelsScore[index].textContent = usersScore[index];
     })
-})
+}
+
 
 function addItemToList(cardList, labelText) {
     let label = document.createElement('label'),
@@ -30,11 +38,21 @@ function addItemToList(cardList, labelText) {
     })
 }
 
+// function recalScore(indexParent) {
+//     userCards.forEach(userCard) {
+//         labelsScore = 
+//     }
+// }
+
 function removeItem(cardItem) {
     let parent = cardItem.parentNode;
     let indexParent = [...cardLists].indexOf(parent);
     data[indexParent] = data[indexParent].filter(el => el !== cardItem.innerText);
     saveData(data);
+
+    usersScore[indexParent] += 1;
+    labelsScore[indexParent].textContent = usersScore[indexParent];
+    localStorage.setItem('usersScore', JSON.stringify(usersScore));
     cardItem.remove();
 }
 
@@ -58,19 +76,30 @@ textField.addEventListener("keyup", function(event) {
 });
 
 function renderAndSave() {
-    if (textField.value === '' || textField.value.trim() === '') {
-        alert('Ты не пройдешь!');
-    } else {
+    let isTextFieldCorrect = validateTextField();
+
+    if (isTextFieldCorrect) {
         for (let [index, cardList] of cardLists.entries()) {
             addItemToList(cardList, textField.value);
             data[index].push(textField.value);
         }
         saveData(data);
+    } else {
+        alert('Ты не пройдешь!');
     }
-    
+}
+
+function validateTextField() {
+    if (textField.value === '' || textField.value.trim() === '') {
+        return false;
+    }
+
+    return true;
 }
 
 function saveData(data) {
     localStorage.setItem('usersData', JSON.stringify(data));
 }
 
+
+initRender();
